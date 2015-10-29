e340948fa2839067849ccc7c47b72cb4dc518dda
Colombo Simone <simone.colombo@mis.mediobanca.com>
2015-10-29 15:30:58 +0100

    Modificato il nome del check

--- wato/mssql_logfile_custom_check_parameters.py
+++ wato/mssql_logfile_custom_check_parameters.py
@@ -1,16 +1,16 @@
 register_check_parameters(
     _("Applications, Processes & Services"),
-    "solaris_ldom_stat",
+    "mssql_percent_log_used",
     _("MSSQL Log file usage"),
     Dictionary(
         elements = [
-            ("usage_levels",
+            ("log_usage_levels",
                 Tuple(
                     title = _("Levels for log usage (%)"),
                     label = _("Levels for log usage (%)"),
                     elements = [
-                        Percentage(title = _("Warning at:" ), maxvalue = 1500.0),
-                        Percentage(title = _("Critical at:"), maxvalue = 1500.0),
+                        Percentage(title = _("Warning at:" ), maxvalue = 15000.0),
+                        Percentage(title = _("Critical at:"), maxvalue = 15000.0),
                     ]
                 ),
             ),
