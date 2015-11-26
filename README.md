05fffc6a67ccd2854edf1f067ff66e746319c0a3
Colombo Simone <simone.colombo@mis.mediobanca.com>
2015-11-26 11:33:55 +0100

    Implementati nuovi check su richiesta dei DBA

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-This plugin is a work in progress
+# MSSQL Checks
+
+Aggiungiamo vari check sui contatori MSSQL come da richiesta dei DBA.
+
+## Check creati
+
+Questi sono i singoli check implementati in questo package. Per tutte le metriche
+che vengono riportate per singolo DB, le righe relative a "_Total" e
+"mssqlsystemresource" vengono ignorate automaticamente. Tutti i check che
+lavorano a livello di singolo DB, supportano una singola regola di inventory
+che scarta automaticamente i due item.
+
+### process_blocked
+
+General Statistic per l'istanza, un solo check creato per istanza. Soglie fisse
+(no media, counter, predictive), ma la specifica dei DBA recita:
+
+> è necessario che appena vi è un processo bloccato per più di 5 minuti sia
+> aperto un warning e dopo 15 minuti un critical
+
+Questo comportamento deve essere implementato su Nagios/BSM
+
+### percent_log_usage
+
+Cattura della metrica così come è riportata da SQL Server: un valore per ogni DB
+gestito da ogni istanza, quindi creiamo 1 check per ogni coppia Istanza/DB.
+
+Nessuna soglia, utile solo per i perfdata.
+
+### active_transactions
+
+Cattura della metrica così come è riportata da SQL Server: un valore per ogni DB
+gestito da ogni istanza, quindi creiamo 1 check per ogni coppia Istanza/DB.
+
+Nessuna soglia, utile solo per i perfdata.
+
+### temp_table_creation_rate
+
+General Statistic per l'istanza, un solo check creato per istanza. Nessuna
+soglia, utile solo per i perfdata.
--- wato/mssql_logfile_custom_check_parameters.py
+++ wato/mssql_logfile_custom_check_parameters.py
@@ -1,16 +1,16 @@
 register_check_parameters(
     _("Applications, Processes & Services"),
-    "mssql_percent_log_used",
-    _("MSSQL Log file usage"),
+    "mssql_processes_blocked_counters",
+    _("MSSQL Blocked Processes"),
     Dictionary(
         elements = [
-            ("log_usage_levels",
+            ("num_blocked",
                 Tuple(
-                    title = _("Levels for log usage (%)"),
-                    label = _("Levels for log usage (%)"),
+                    title = _("Number of Blocked Processes"),
+                    label = _("Number of Blocked Processes"),
                     elements = [
-                        Percentage(title = _("Warning at:" ), maxvalue = 15000.0),
-                        Percentage(title = _("Critical at:"), maxvalue = 15000.0),
+                        Integer(title = _("Warning at:" ), maxvalue = 15000),
+                        Integer(title = _("Critical at:"), maxvalue = 15000),
                     ]
                 ),
             ),
@@ -22,4 +22,24 @@ register_check_parameters(
         allow_empty = True,
     ),
     "dict"
+)
+
+register_rule("checkparams" + "/" +  _("Inventory - automatic service detection"),
+    # This is the name of the variable that this rule will populate
+    # THE VARIABLE MUST BE DECLARED ALSO IN THE CHECK, wher it will be used
+    # in the inventory function
+    varname     = "unwanted_sql_dbs",
+    # Label for the variable
+    title         = _("SQL Server counters discovery"),
+    # Structure of the data the variable will hold
+    valuespec = ListOf(
+        TextAscii(
+            title = _("Database"),
+            help  = _( "Name of the database that you want to ignore" ),
+            default_value = "_Total",
+        ),
+        movable = False,
+        title = _("List of DBs to ignore")
+    ),
+    match= 'all',
 )
